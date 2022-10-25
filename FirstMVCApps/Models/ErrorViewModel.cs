e350f69c68d4b8f2a3fa9107eb0f3755777d254e
Mohammed Nazim Uddin <mdnazimuddincse7@gmail.com>
2022-10-25 12:54:18 +0600

    init

---
+++ FirstMVCApps/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstMVCApps.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
